c613203d29362959791d0a1a458ffc5a170a15ec
wk-j <somnuk.wk@gmail.com>
2018-06-05 23:22:22 +0700

    Json module

---
+++ src/Features/29/Keyof/Keyof.ts
@@ -0,0 +1,25 @@
+function kkeyof() {
+    const c = "c"
+    const d = 10
+    const e = Symbol()
+    const enum E1 { A, B, C };
+    const enum E2 { A = "A", "B" = "B", C = "C" };
+
+    type Foo = {
+        a: string
+        5: string
+        [c]: string
+        [d]: string
+        [e]: string
+        [E1.A]: string
+        [E2.A]: string
+    }
+
+    type K1 = keyof Foo
+    type K2 = Extract<keyof Foo, string>
+    type K3 = Extract<keyof Foo, number>
+    type K4 = Extract<keyof Foo, symbol>
+}
+
+kkeyof();
+
