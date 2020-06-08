70d748c83a25dbf0d693e627f3d9133a36e3ba80
wk <somnuk.wk@gmail.com>
2020-06-09 02:24:11 +0700

    Conditional type

---
+++ src/Tests/T.ts
@@ -0,0 +1,47 @@
+// https://www.facebook.com/imn3tr/posts/10160172168394782?__cft__[0]=AZWLzwYL5jd4bgpK7IA6ZHplHjFaC_C9ubPpMuFs_XIAHrTadCbekdJrJ3GsQ7FuyPN0HFWuW2mgRSk8cAANCzYzYT8wMnnIoEkNX1w_s6b4jK6kJGrNz-yorTisfkqkEUWWlXv4edLgkc0xbatm-hlU&__tn__=%2CO%2CP-R
+
+namespace TK {
+
+    type InputA = {
+        type: "A"
+    }
+
+    type InputB = {
+        type: "B",
+    }
+
+    type OptionA = {
+        optionA: string
+    }
+
+    type OptionB = {
+        optionB: string
+    }
+
+    type Kind = {
+        A: { input: InputA, options: OptionA },
+        B: { input: InputB, options: OptionB }
+    }
+
+    type Key = <T extends keyof Kind>() => T
+
+    type KReturn = <T extends keyof Kind>(input: { type: T } & Kind[T]["input"]) => Kind[T]["options"]
+
+    let process: KReturn = (value) => {
+        if (value.type == "A") {
+            return { optionA: "A" }
+        } else {
+            return { optionB: "B" }
+        }
+    }
+
+    var a = process({ type: "A" })
+    var b = process({ type: "B" })
+
+    export function go() {
+        console.log(a);
+        console.log(b);
+    }
+}
+
+TK.go()
